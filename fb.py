bb88d846311b53ef4f4567712a720de9f227373e
Animesh452 <animeshk452@gmail.com>
2022-10-01 12:59:56 +0530

    hopeweb
    hope

---
+++ fb.py
@@ -0,0 +1,21 @@
+import sys
+import requests
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import storage
+
+image_url = sys.argv[1] #we pass the url as an argument
+
+cred = credentials.Certificate('path/to/certificate.json')
+firebase_admin.initialize_app(cred, {
+    'storageBucket': '<mysuperstorage>.appspot.com'
+})
+bucket = storage.bucket()
+
+image_data = requests.get(image_url).content
+blob = bucket.blob('new_cool_image.jpg')
+blob.upload_from_string(
+        image_data,
+        content_type='image/jpg'
+    )
+print(blob.public_url)
