59d51913d71efcad1d6f01f5e595283ad9f3f890
Animesh452 <68946005+Animesh452@users.noreply.github.com>
2022-10-05 16:31:40 +0530

    Update Deploy_s2b.py

--- Deploy_s2b.py
+++ Deploy_s2b.py
@@ -230,8 +230,8 @@ class Paper:
 
         img=self.name + " pg" + str(self._page) + ".png"
         storage.child(img).put(img)
-        imgUrl = storage.child(img).get_url(user['idToken'])
-        st.write(imgUrl)
+#         imgUrl = storage.child(img).get_url(user['idToken'])
+#         st.write(imgUrl)
 
 
     def show(self):
